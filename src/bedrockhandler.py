1f1f14b18ded569e094c48262572dbee8927a4a8
mohsenabedelaal <mohsen.abedelaal@gmail.com>
2024-08-08 13:23:43 +0300

    add the logic that put the generated image into s3 bucket.

--- src/bedrockhandler.py
+++ src/bedrockhandler.py
@@ -1,6 +1,8 @@
 import boto3
 from botocore.config import Config
 import json
+import base64
+import datetime
 
 bedrock_config = Config(
     region_name = "us-west-2"
@@ -26,7 +28,23 @@ def handler(event,context):
     print(response_bedrock)
     response_bedrock_byte = json.loads(response_bedrock['body'].read())
     print("response in bytes")
-    print(response_bedrock_byte)
+    # print(response_bedrock_byte)
+    response_bedrock_base64 = response_bedrock_byte['artifacts'][0]['base64']
+    response_bedrock_finalimage = base64.b64decode(response_bedrock_base64)
+    print("response bedrock final image")
+    print(response_bedrock_finalimage)
+    poster_name = f"posterName-{datetime.datetime.today().strftime('%Y-%M-%D-%M-%S')}"
+    bucket_name = 'movieposterdesignstorebucket'
+    response_s3 = client_s3.put_object(
+        Bucket= bucket_name,
+        Key=poster_name,
+        Body=response_bedrock_finalimage
+    )
+    generate_presigned_url = client_s3.generate_presigned_url('get_object', 
+                                                              Params={'Bucket':bucket_name,'Key':poster_name}, 
+                                                              ExpiresIn=3600)
+    print("generate presigned")
+    print(generate_presigned_url)
     return {
         'status':200,
         'body':json.dumps('Hello From Lambda')
